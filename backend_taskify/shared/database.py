148aa50332d50467df324e70b2831cda3c7f63a6
Moisés Corbellari <moisescorbellari1@outlook.com>
2026-04-18 22:21:43 -0300

    chore(databese.py): remove imports antigos

--- backend_taskify/shared/database.py
+++ backend_taskify/shared/database.py
@@ -1,9 +1,7 @@
 from dotenv import load_dotenv
 import os
-# from sqlalchemy import create_engine
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-# from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
 
