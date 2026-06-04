41b7a60c344a19ee6797cb8d9143445b9152c49b
Moisés Corbellari <moisescorbellari1@outlook.com>
2026-06-04 00:05:12 -0300

    refactor: separa endpoints de tarefas em módulos

--- backend_taskify/project_taskify/routers/task_router.py
+++ backend_taskify/project_taskify/routers/task_router.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter
-from .service.get_all import router as get_all_task
-from .service.get_id import router as get_task_by_id
-from .service.create import router as create_task
-from .service.update import router as update_task_by_id
-from .service.finish import router as finish_task_by_id
-from .service.delete import router as delete_task_by_id
+from .service.get_all import router as get_all_router
+from .service.get_id import router as get_by_id_router
+from .service.create import router as create_router
+from .service.update import router as update_router
+from .service.finish import router as finish_router
+from .service.delete import router as delete_router
 
 
 
@@ -13,9 +13,9 @@ router = APIRouter(
     tags=["Lista de tarefas"]
     )
 
-router.include_router(get_all_task)
-router.include_router(get_task_by_id)
-router.include_router(create_task)
-router.include_router(update_task_by_id)
-router.include_router(finish_task_by_id)
-router.include_router(delete_task_by_id)
+router.include_router(get_all_router)
+router.include_router(get_by_id_router)
+router.include_router(create_router)
+router.include_router(update_router)
+router.include_router(finish_router)
+router.include_router(delete_router)
