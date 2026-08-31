eee867b5901be76801e335121bc97ac794ebccd4
Moisés Corbellari <moisescorbellari1@outlook.com>
2026-08-31 14:53:24 -0300

    chore: atualiza dependencias uv.lock

--- backend/app/models/task_model.py
+++ backend/app/models/task_model.py
@@ -7,8 +7,27 @@ from sqlalchemy import Boolean, Column, Date, Integer, String, Text
 class Task(Base):
     __tablename__ = "tasks"
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    title = Column(String(255), nullable=False)
-    description = Column(Text, nullable=True)
-    created = Column(Date, nullable=False, default=date.today)
-    completed = Column(Boolean, nullable=False, default=False)
+    id = Column(
+        Integer, 
+        primary_key=True, 
+        autoincrement=True)
+    
+    title = Column(
+        String(255), 
+        nullable=False)
+    
+    description = Column(
+        Text, 
+        nullable=True)
+    
+    created = Column(
+        Date, 
+        nullable=False, 
+        default=date.today)
+    
+    completed = Column(
+        Boolean, 
+        nullable=False, 
+        default=False)
+
+    
